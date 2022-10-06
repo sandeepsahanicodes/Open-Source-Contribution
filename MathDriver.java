224928c4ff0117335add93df6d86340e36ed498d
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2022-10-06 12:06:17 +0530

    Add methods

--- MathDriver.java
+++ MathDriver.java
@@ -1,10 +1,22 @@
 // Math class
-class Math
+class MyMath
 {
-   
+   public static int add(int a,int b)
+   {
+       return a+b;
+   }
+
+   public static int sub(int a,int b)
+   {
+       return a-b;
+   }
 }
 // Driver class
 class MathDriver
 {
-  
+    public static void main(String[] args) 
+    {
+        System.out.println(MyMath.add(10, 20));
+        System.out.println(MyMath.sub(10, 20));
+    }
 }
