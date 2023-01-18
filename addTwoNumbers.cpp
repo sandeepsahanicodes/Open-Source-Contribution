8fb00beb586cd14c846821c2247d987b68caffdd
Amarnath Aich <amarnathaich@gmail.com>
2023-01-19 00:30:36 +0530

    added code to add two numbers

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,r=0;
+    printf("This is a addition calculater\n");
+    printf("Enter the First Value :");
+    scanf("%d",&a);
+    printf("Enter the Second Value :");
+    scanf("%d",&b);
+    r=(a+b);
+    printf("The total value is = %d",r);
+    
+}
+
